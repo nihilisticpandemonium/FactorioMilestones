b9fb77f0060045b552632aeaa76afce665c72353
Nihilistzsche <nihilistzsche@gmail.com>
2023-01-09 03:45:24 -0800

    Add a dynamic addon preset for Infused Science (Fixed) and a framework for dynamic presets.

--- presets/preset_addons.lua
+++ presets/preset_addons.lua
@@ -303,6 +303,22 @@ preset_addons = {
         }
     },
 
+    ["Infused Science (Fixed)"] = {
+        required_mods = {"infused-science-fixed"},
+        milestones = {
+          {type = "group", name = "Science"}
+        },
+        build_func = function(self)
+          for _, science in pairs(game.item_prototypes) do
+            if science and science.type == "tool" and science.name:sub(1, 8) == "infused-" then
+              log("Adding milestone for: " .. science.name)
+              table.insert(self.milestones, {type = "item", name = science.name, quantity = 1})
+              table.insert(self.milestones, {type = "item", name = science.name, quantity = 1000, next = "x10"})
+            end
+          end
+        end
+    },
+
     ["Brass Tacks"] = {
         required_mods = {"BrassTacks"},
         milestones = {
